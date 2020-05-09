a2df79fd9ed83190055d6a44e8939ec2b46da6ec
Lasse <lasse.kreuter@iem.thm.de>
2020-05-09 21:27:02 +0200

    Erste onClick funktionen umgesetzt

--- my-app/src/components/timeLineItem.js
+++ my-app/src/components/timeLineItem.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import entry from "./entries";
 
-const TimeLineItem = ({entries}) => (
-    <div className="timeline-item">
-        <div className="timeline-item-content">
+
+
+function TimeLineItem ({entries}) {
+    function clickHandler() {
+        let eingabe=prompt("Inhalt eingeben.", "");
+        if (eingabe!=""){
+            setText(eingabe);
+        }
+    }
+    const [text, setText] = useState(entries.text);
+    const onChange = event => setText(event.target.text);
+    return (
+        <div className="timeline-item">
+            <div className="timeline-item-content">
             <span className="tag" style={{background: entries.category.color}}>
                 {entries.category.tag}
             </span>
-            <time>{entries.date}</time>
-            <p>{entries.text}</p>
-            <div className="circles">
-                <span className="circle1"/>
-                <span className="circle2"/>
-                <span className="circle3"/>
+                <time>{entries.date}</time>
+                <p>{text}</p>
+                <div className="circles" onClick={() => clickHandler()}>
+                    <span className="circle1"/>
+                    <span className="circle2"/>
+                    <span className="circle3"/>
+                </div>
             </div>
         </div>
-    </div>
+    )
+}
 
-);
 export default TimeLineItem;
