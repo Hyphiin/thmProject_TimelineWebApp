37600c7808dd4c998ca3de5bca7be421d0af6faa
Felix Wegener <felix.wegener@iem.thm.de>
2020-05-18 15:22:42 +0200

    Icons in der Toolbox anwählbar

--- my-app/src/components/toolBox.js
+++ my-app/src/components/toolBox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Add from "./Add";
+import IconInsert from "../IconInsert";
 
 function Toolbox () {
 
@@ -7,6 +8,7 @@ function Toolbox () {
     <div id="toolbox_container">
         <p id="toolbox_header">Toolbox</p>
         <Add/>
+        <IconInsert/>
     </div>
     )
 }
