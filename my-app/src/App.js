8b2690f72bb5d1adb4cff665c8a41b8d8079b9c0
Sandra <sandra.schmidt@iem.thm.de>
2020-05-27 17:55:33 +0200

    Ausprobieren der Implementierung

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 import Timeline from "./components/timeLine";
 import Toolbox from "./components/toolBox";
 import Screenshot from "./components/screenCapture";
+import localStorageCall from "./components/localStorageCall";
 
 const App = () => (
     <>
+
         <h1>Max Mustermann</h1>
         <div className="container">
         <Timeline/>
         <Toolbox/>
         </div>
         <Screenshot/>
-    </>
+        <localStorageCall/>
+   </>
+
+
 );
+
 
 export default App;
---
+++ my-app/src/components/localStorageCall.js
@@ -0,0 +1,33 @@
+import React from "react";
+import useStateWithLocalStorage from "./useStateWithLocalStorage";
+
+
+ const localStorageCall = () => (
+
+    <button id="localStorage-Button" onClick={useStateWithLocalStorage}>
+        Speichern
+    </button>
+
+);
+
+{/*
+const [value, setValue] = useStateWithLocalStorage("myValueLocal"); //value has to be the whole Site somehow
+const onClick = event => setValue(event.target.value);
+*/}
+
+{/*} const useStateWithLocalStorage = localStorageKey => {
+        const [value, setValue] = React.useState(localStorage.getItem(localStorageKey) || "");
+
+        React.useEffect(() => {
+            localStorage.setItem(localStorageKey, value);
+        }, [value]);
+
+        return [value,setValue];
+    };
+
+        const [value, setValue] = useStateWithLocalStorage("myValueLocal");
+        const onClick = event => setValue(event.target.value);
+
+
+*/}
+export default localStorageCall;
