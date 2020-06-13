16fcbae7754bc138bd0c887c45dded67a1ac0306
Sandra <sandra.schmidt@iem.thm.de>
2020-06-13 22:27:27 +0200

    Mittlerer Punkt

--- my-app/src/components/timeLineItem.js
+++ my-app/src/components/timeLineItem.js
@@ -8,8 +8,8 @@ import {
 } from "./Animation";
 
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
     faCoffee,
     faBirthdayCake,
@@ -47,21 +47,21 @@ library.add(
 );
 
 
-function TimeLineItem ({entries}) {
+function TimeLineItem({entries}) {
 
-    function clickHandler(mode,pos) {
-        localStorage.setItem("mode","neutral");
-        if (mode==="add"&& pos==="before") {
-            editContent(localStorage.getItem("text"), localStorage.getItem("date"), localStorage.getItem("tag"), localStorage.getItem("icon"), localStorage.getItem("file"), editContent("","","","","","","get").indexOf(entries),"add");
+    function clickHandler(mode, pos) {
+        localStorage.setItem("mode", "neutral");
+        if (mode === "add" && pos === "before") {
+            editContent(localStorage.getItem("text"), localStorage.getItem("date"), localStorage.getItem("tag"), localStorage.getItem("icon"), localStorage.getItem("file"), editContent("", "", "", "", "", "", "get").indexOf(entries), "add");
         }
-        if (mode==="add" && pos==="after"){
-            editContent(localStorage.getItem("text"), localStorage.getItem("date"), localStorage.getItem("tag"), localStorage.getItem("icon"), localStorage.getItem("file"), editContent("","","","","","","get").indexOf(entries)+1,"add");
+        if (mode === "add" && pos === "after") {
+            editContent(localStorage.getItem("text"), localStorage.getItem("date"), localStorage.getItem("tag"), localStorage.getItem("icon"), localStorage.getItem("file"), editContent("", "", "", "", "", "", "get").indexOf(entries) + 1, "add");
         }
-        if (mode==="delete"){
-            editContent("","","","","",editContent("","","","", "","","get").indexOf(entries),"delete");
+        if (mode === "delete") {
+            editContent("", "", "", "", "", editContent("", "", "", "", "", "", "get").indexOf(entries), "delete");
         }
-        if (mode==="edit"){
-            editContent(localStorage.getItem("text"), localStorage.getItem("date"), localStorage.getItem("tag"), localStorage.getItem("icon"), localStorage.getItem("file"),editContent("","","","", "", "","get").indexOf(entries),"edit");
+        if (mode === "edit") {
+            editContent(localStorage.getItem("text"), localStorage.getItem("date"), localStorage.getItem("tag"), localStorage.getItem("icon"), localStorage.getItem("file"), editContent("", "", "", "", "", "", "get").indexOf(entries), "edit");
         }
     }
 
@@ -75,32 +75,40 @@ function TimeLineItem ({entries}) {
 
     return (
         <div className="timeline-item">
-            <div className="timeline-item-content" >
+            <div className="timeline-item-content">
                 <div className="box">
-            <span className="tag" >
+            <span className="tag">
                 {entries.tag}
             </span>
-                <time>{entries.date}</time>
-                <p onClick={() => clickHandler("remove")}> {entries.text} </p>
-                <FontAwesomeIcon className="test" icon={entries.icon} />
-                <div>
-                <img className="image" src={entries.file}/>
-                </div>
+                    <time>{entries.date}</time>
+                    <p onClick={() => clickHandler("remove")}> {entries.text} </p>
+                    <FontAwesomeIcon className="test" icon={entries.icon}/>
+                    <div>
+                        <img className="image" src={entries.file}/>
+                    </div>
                 </div>
                 <div ref={el => (fileA = el)} className="file-input-content">
-                <span onMouseEnter={e => hoverCircle(e)}
-                      onMouseOut={e => hoverExitCircle(e)} ref={el => (circle1A = el)}
-                      className="circle1" onClick={() => {clickHandler(localStorage.getItem("mode"),"before"); LocalStorageSave()}}/>
-                <span onMouseEnter={e => hoverCircle(e)}
-                      onMouseOut={e => hoverExitCircle(e)} ref={el => (circle1A = el)}
-                      ref={el => (circle2A = el)} className="circle2" onClick={() => {clickHandler(localStorage.getItem("mode"),"after"); LocalStorageSave()}}/>
-                    {/* ????
-                      <span onMouseEnter={e => hoverCircle(e)}
-                      onMouseOut={e => hoverExitCircle(e)} ref={el => (circle1A = el)}
-                      ref={el => (circle2A = el)} className="circle3" onClick={() => {clickHandler(localStorage.getItem("mode"),"after"); LocalStorageSave()}}/>
-                      */}
+                    <span onMouseEnter={e => hoverCircle(e)}
+                          onMouseOut={e => hoverExitCircle(e)} ref={el => (circle1A = el)}
+                          className="circle1" onClick={() => {
+                        clickHandler(localStorage.getItem("mode"), "before");
+                        LocalStorageSave()
+                    }}/>
+                    <span onMouseEnter={e => hoverCircle(e)}
+                          onMouseOut={e => hoverExitCircle(e)} ref={el => (circle1A = el)}
+                          ref={el => (circle2A = el)} className="circle2" onClick={() => {
+                        clickHandler(localStorage.getItem("mode"), "after");
+                        LocalStorageSave()
+                    }}/>
+                    <span onMouseEnter={e => hoverCircle(e)}
+                          onMouseOut={e => hoverExitCircle(e)} ref={el => (circle1A = el)}
+                          ref={el => (circle2A = el)} className="circle3" onClick={() => {
+                        clickHandler(localStorage.getItem("mode"), "after");
+                        LocalStorageSave()
+                    }}/>
+
                 </div>
-        </div>
+            </div>
         </div>
     )
 }
