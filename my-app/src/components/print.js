09eafce65d50b1451911f13181f37ad187f1747d
Chiara <ck@rswz.de>
2020-06-25 20:39:09 +0200

    PrintThisOutButton

--- my-app/src/components/print.js
+++ my-app/src/components/print.js
@@ -1,14 +1,17 @@
 import React, {useRef} from "react";
 import ReactToPrint from "react-to-print";
 import Timeline from "./timeLine.js";
+import {hoverButton, hoverErstellenButton, hoverExitButton, hoverExitErstellenButton} from "./Animation";
 
         const Print = () => {
             const componentRef = useRef();
 
             return (
-                <div>
+                <div class="printButton">
                     <ReactToPrint
-                        trigger={() => <button>Print this out!</button>}
+                        trigger={() => <button class="button"
+                                               onMouseEnter={e => hoverButton(e)}
+                                               onMouseOut={e => hoverExitButton(e)}>Print this out!</button>}
                         content={() => componentRef.current}
                     />
                     <div style={{display: "none"}}>
