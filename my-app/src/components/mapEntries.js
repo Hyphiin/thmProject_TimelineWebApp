21ee33648a709b87fb3e5f5dbd36962874126149
Robin Patzak <robin.patzak@iem.thm.de>
2020-06-25 19:10:49 +0200

    Neuer Ansatz mit Library Beautiful DnD

--- my-app/src/components/mapEntries.js
+++ my-app/src/components/mapEntries.js
@@ -3,26 +3,41 @@ import editContent from "./AddEntries";
 import TimeLineItem from "./timeLineItem";
 import LocalStorageSave from "./LocalStorageSave";
 import LocalStorageUsage from "./localStorageUsage";
+import {Draggable} from "react-beautiful-dnd";
 
-class MapEntries extends React.Component{
 
-    render()
-    {
-        let i=parseInt(localStorage.getItem("nuOfTimelines"));
-        if(isNaN(i)===true){
+class MapEntries extends React.Component {
+
+    render() {
+        let i = parseInt(localStorage.getItem("nuOfTimelines"));
+        if (isNaN(i) === true) {
             console.log("drinne");
-            i=1;
-            localStorage.setItem("nuOfTimelines",i.toString());
-            localStorage.setItem("position",i.toString());
-            editContent("","","","", "","","new");
+            i = 1;
+            localStorage.setItem("nuOfTimelines", i.toString());
+            localStorage.setItem("position", i.toString());
+            editContent("", "", "", "", "", "", "new");
         }
 
-            return editContent("", "", "", "", "","","get").map((entries, idx) => (
-                <TimeLineItem entries={entries} key={idx}/>
+        return (
+            editContent("", "", "", "", "", "", "get").map((entries, idx) => (
+                <Draggable draggableId={entries} index={idx}>
+                    {(provided) => (
+                        <TimeLineItem
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            innerRef={provided.innerRef}
+                            entries={entries} key={idx}>
+                        </TimeLineItem>
+                    )}
+
+                </Draggable>
+
             ))
-        }
+        )
+    }
 
 }
+
 export default MapEntries;
 
 
--- my-app/src/components/timeLine.js
+++ my-app/src/components/timeLine.js
@@ -7,24 +7,45 @@ import {
     timelineStagger,
     tlAnimationRight
 } from "./Animation";
+import {DragDropContext, Droppable} from "react-beautiful-dnd";
 
 
 console.log("timeline");
 
- class Timeline extends React.Component{
+class Timeline extends React.Component {
 
-     constructor(props) {
-         super(props);
-         this.state = {
-             entries: this.props.entries
-         }
-     }
+    constructor(props) {
+        super(props);
+        this.state = {
+            entries: this.props.entries
+        }
+    }
 
-     render() {
-         return <div id="timeline-export" className="timeline-container">
-             <MapEntries/>
-         </div>
-     }
+    onDragEnd = (e) => {
+
+    }
+
+    render() {
+        return (
+            editContent("", "", "", "", "", "", "get").map((entries, idx) => (
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                    <Droppable droppableId={entries}>
+                        {(provided) => (
+                            <div
+                                innerRef={provided.innerRef}
+                                {...provided.droppableProps}
+                                id="timeline-export" className="timeline-container">
+                                <MapEntries/>
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+
+            ))
+        )
+
+    }
 
 
 }
